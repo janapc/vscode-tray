1b7ee09621278560f78bd7590a9c0fe3e0621d17
janaina pedrina <janapc@MacBook-Pro-de-janaina.local>
2020-02-15 17:25:28 -0300

    feat: project with tray of electron

---
+++ README.md
@@ -0,0 +1,25 @@
+# Description
+this project go created for easing the way how we open the projects with vscode.
+
+# Install
+
+install and execute:
+```
+  yarn && yarn electron
+```
+
+build for mac:
+```
+  yarn && yarn dist:mac
+```
+
+build for windows:
+```
+  yarn && yarn dist:windows
+```
+
+build for linux:
+```
+  yarn && yarn dist:linux
+```
+
---
+++ main.js
@@ -0,0 +1,98 @@
+const { resolve, basename } = require('path');
+const {
+  app, Menu, Tray, dialog,
+} = require('electron');
+const Store = require('electron-store');
+
+const { spawn } = require('child_process');
+
+const schema = {
+  projects: {
+    type: 'string',
+  },
+};
+
+const store = new Store({ schema });
+let mainTray = {};
+
+if (app.dock) app.dock.hide();
+
+function render(tray = mainTray) {
+  const storedProjects = store.get('projects');
+  const projects = storedProjects ? JSON.parse(storedProjects) : [];
+
+  const items = projects.map(({ name, path }) => ({
+    label: name,
+    submenu: [
+      {
+        label: 'open vscode',
+        click: () => {
+          spawn('code', [path], { shell: true });
+        },
+      },
+      {
+        label: 'remove project',
+        click: () => {
+          store.set(
+            'projects',
+            JSON.stringify(projects.filter((item) => item.path !== path)),
+          );
+          render();
+        },
+      },
+    ],
+  }));
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'add new project',
+      click: () => {
+        const result = dialog.showOpenDialogSync({
+          properties: ['openDirectory'],
+        });
+
+        if (!result) return;
+
+        const [path] = result;
+        const name = basename(path);
+
+        store.set(
+          'projects',
+          JSON.stringify([
+            ...projects,
+            {
+              path,
+              name,
+            },
+          ]),
+        );
+
+        render();
+      },
+    },
+    {
+      type: 'separator',
+    },
+    ...items,
+    {
+      type: 'separator',
+    },
+    {
+      type: 'normal',
+      label: 'close',
+      role: 'quit',
+      enabled: true,
+    },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+
+  tray.on('click', tray.popUpContextMenu);
+}
+
+
+app.on('ready', () => {
+  mainTray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
+
+  render(mainTray);
+});
